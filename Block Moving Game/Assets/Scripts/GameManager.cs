9b43039a798acb04c409db7ae006ae53cb7bd968
Monochromaticz <juanmartinez2651@gmail.com>
2024-09-30 16:36:00 -0400

    Fixed Movement
    -fixed movement so now it moves with mouse
    -Prefabs for different block types

---
+++ Block Moving Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private TileController selectedTile;
+    private Vector2 touchStartPos;
+    private Vector2 touchEndPos;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SelectTile();
+            touchStartPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0) && selectedTile != null)
+        {
+            touchEndPos = Input.mousePosition;
+            DetectSwipe();
+            selectedTile = null;
+        }
+    }
+
+    private void SelectTile()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+        if (hit.collider != null && hit.collider.CompareTag("Tile"))
+        {
+            selectedTile = hit.collider.GetComponent<TileController>();
+        }
+    }
+
+    private void DetectSwipe()
+    {
+        Vector2 swipeDelta = touchEndPos - touchStartPos;
+
+        swipeDelta.Normalize();
+        if (Mathf.Abs(swipeDelta.x) > Mathf.Abs(swipeDelta.y))
+        {
+            if (swipeDelta.x > 0 && selectedTile.CanMove(Vector2.right))
+            {
+                selectedTile.MoveTile(Vector2.right);
+            }
+            else if (swipeDelta.x < 0 && selectedTile.CanMove(Vector2.left))
+            {
+                selectedTile.MoveTile(Vector2.left);
+            }
+        }
+        else
+        {
+            if (swipeDelta.y > 0 && selectedTile.CanMove(Vector2.up))
+            {
+                selectedTile.MoveTile(Vector2.up);
+            }
+            else if (swipeDelta.y < 0 && selectedTile.CanMove(Vector2.down))
+            {
+                selectedTile.MoveTile(Vector2.down);
+            }
+        }
+    }
+}
--- Block Moving Game/Assets/Scripts/TileController.cs
+++ Block Moving Game/Assets/Scripts/TileController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TileController : MonoBehaviour
 {
@@ -7,89 +6,36 @@ public class TileController : MonoBehaviour
     public Transform checkDown;
     public Transform checkLeft;
     public Transform checkRight;
-    private Collider2D tileCollider; 
 
-
-    public Vector2 checkSize = new Vector2(1f, 1f); 
-    public float moveDistance = 1f; 
-    public GameObject buttonPrefab; 
-    public Canvas canvas; 
-
-    private void Start()
+    public void Start()
     {
-        tileCollider = GetComponent<Collider2D>(); 
-        HideMovementOptions();
+        SetCheckPositionsActive(false);
     }
-
-    private void OnMouseDown()
+    public void MoveTile(Vector2 direction)
     {
-        ShowMovementOptions();
-    }
-
-    private void ShowMovementOptions()
-    {
-        tileCollider.enabled = false;
-        if (CanMove(Vector2.up)) CreateButton(Vector2.up, new Vector3(0, 50, 0));   
-        if (CanMove(Vector2.down)) CreateButton(Vector2.down, new Vector3(0, -50, 0)); 
-        if (CanMove(Vector2.left)) CreateButton(Vector2.left, new Vector3(-50, 0, 0)); 
-        if (CanMove(Vector2.right)) CreateButton(Vector2.right, new Vector3(50, 0, 0)); 
-    }
-
-
-    private void CreateButton(Vector2 direction, Vector3 offset)
-    {
-        GameObject button = Instantiate(buttonPrefab, canvas.transform);
-
-        Vector3 canvasPosition = Camera.main.WorldToScreenPoint(transform.position);
-
-        RectTransform canvasRectTransform = canvas.GetComponent<RectTransform>();
-        Vector2 localPoint;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, canvasPosition, Camera.main, out localPoint);
-
-        Vector2 buttonPosition = localPoint + (Vector2)offset;
-
-        button.GetComponent<RectTransform>().anchoredPosition = buttonPosition;
-
-        Button btn = button.GetComponent<Button>();
-        btn.onClick.AddListener(() => MoveTile(direction));
-        btn.onClick.AddListener(HideMovementOptions); 
-    }
-
-
-
-    private void MoveTile(Vector2 direction)
-    {
-        Vector3 newPosition = transform.position + (Vector3)direction * moveDistance;
+        Vector3 newPosition = transform.position + (Vector3)direction; 
         transform.position = newPosition; 
     }
 
-    private void HideMovementOptions()
-    {
-        foreach (Transform child in canvas.transform)
-        {
-            if (child.CompareTag("DirButton"))
-            {
-                Destroy(child.gameObject);
-            }
-        }
-        tileCollider.enabled = true;
-    }
-
-    private bool CanMove(Vector2 direction)
+    public bool CanMove(Vector2 direction)
     {
         Transform checkPosition = null;
-
         if (direction == Vector2.up) checkPosition = checkUp;
         else if (direction == Vector2.down) checkPosition = checkDown;
         else if (direction == Vector2.left) checkPosition = checkLeft;
         else if (direction == Vector2.right) checkPosition = checkRight;
 
-        // Check for collisions
-        Collider2D[] hits = Physics2D.OverlapBoxAll(checkPosition.position, checkSize, 0f);
+        BoxCollider2D checkCollider = checkPosition.GetComponent<BoxCollider2D>();
+
+        SetCheckPositionsActive(true);
+
+        Collider2D[] hits = Physics2D.OverlapBoxAll(checkPosition.position, checkCollider.size, 0f);
+
+        SetCheckPositionsActive(false);
 
         foreach (var hit in hits)
         {
-            if (hit.CompareTag("Tile"))
+            if (hit.CompareTag("Tile") && hit.gameObject != gameObject) 
             {
                 return false; 
             }
@@ -98,4 +44,11 @@ public class TileController : MonoBehaviour
         return true; 
     }
 
+    private void SetCheckPositionsActive(bool isActive)
+    {
+        checkUp.gameObject.SetActive(isActive);
+        checkDown.gameObject.SetActive(isActive);
+        checkLeft.gameObject.SetActive(isActive);
+        checkRight.gameObject.SetActive(isActive);
+    }
 }
